3a53c360673533f3acd1b4bca35a0d75502e79d5
John Lu <johnlu49@yahoo.com>
2018-04-22 03:38:25 -0700

    Complete buttons

---
+++ buttons.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#button1').click(function() {
+    $('p').text("hey");
+  });
+  $('#button2').click(function() {
+    $('p').text("no");
+  });
+  $('#button3').click(function() {
+    $('p').text("yes");
+  });
+  $('#button4').click(function() {
+    $('p').text("please help me please");
+  });
+});
