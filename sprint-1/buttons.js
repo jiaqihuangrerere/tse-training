4760a7dbffcb428644d5e80e005d25834ed09497
jiaqi huang <huangjiaqi2397056Hjq@gmail.com>
2018-04-22 22:57:08 -0700

    add my files

---
+++ sprint-1/buttons.js
@@ -0,0 +1,19 @@
+//Helloworld
+function getHelloworld() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+
+//I am Jacky
+function getJacky() {
+    document.getElementById("demo").innerHTML = "I am Jacky";
+}
+
+//Helloworld
+function getUCSD() {
+    document.getElementById("demo").innerHTML = "I am a UCSD Student";
+}
+
+//Helloworld
+function getSanDiego() {
+    document.getElementById("demo").innerHTML = "I live in San Diego";
+}
