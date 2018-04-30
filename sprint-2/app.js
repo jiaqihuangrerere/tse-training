2405f5f995f0bf7ef4381c631bcf6f8c2826cf3e
jiaqi huang <huangjiaqi2397056Hjq@gmail.com>
2018-04-30 01:21:23 -0700

    working on routers and app js

--- sprint-2/app.js
+++ sprint-2/app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var about = require('./routes/about');
+var post1 = require('./routes/post1');
+var index = require('./routes/');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -19,5 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/about', about);
+app.use('/post1', post1);
+app.use('/', index);
+app.use('/posts', posts);
 
 module.exports = app;
