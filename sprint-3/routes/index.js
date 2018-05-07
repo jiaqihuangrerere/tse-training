530825638a4dfbbcaf0caf3254309e6faff4c2a8
Arnold Duan <c3duan@ucsd.edu>
2018-05-06 23:01:55 -0700

    Draft

--- sprint-3/routes/index.js
+++ sprint-3/routes/index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var data = require('../chirps.json')
+var data = require('../chirps.json');
+
+var template = $('#handlebars-sprint-3').html();
+
+var html = templateScript(data);
+
+$(document.body).append(html);
 
 router.get('/', function(req, res, next) {
 	res.render('index', data);
